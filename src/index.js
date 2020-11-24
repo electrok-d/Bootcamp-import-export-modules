14a7717f08c2ccba0219f5e65ef644944eb2b017
Jed Black <62964873+electrok-d@users.noreply.github.com>
2020-11-23 19:58:46 -0800

    learned import and export functionality

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import pi, { doublePi, triplePi } from "./math.js";
 
 ReactDOM.render(
   <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
+    <li>{pi}</li>
+    <li>{doublePi()}</li>
+    <li>{triplePi()}</li>
   </ul>,
   document.getElementById("root")
 );
+
+// wildcard is discouraged because the benefit of having a single default export is gone. Individual exports allow for cleaner code
+//import * as pi from "./math.js";
+
+// ReactDOM.render(
+//   <ul>
+//     <li>{pi.default}</li>
+//     <li>{pi.doublePi()}</li>
+//     <li>{pi.triplePi()}</li>
+//   </ul>,
+//   document.getElementById("root")
+// );
